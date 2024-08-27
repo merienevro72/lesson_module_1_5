f78f3a0ffa9a092afbe7293b1949e1039c9fda42
merienevro72 <merienevro72@yandex.ru>
2024-08-27 21:12:21 +0500

    Add files via upload

---
+++ module_1_5.py
@@ -0,0 +1,6 @@
+immutable_var = (1,2,3,'a','b','c')
+print (immutable_var)
+#immutable_var[0] = 5- элементы кортежа являются неизменяемыми
+mutable_list = [1,2,3,'a','b','c']
+mutable_list[3] = 'A'
+print (mutable_list)
